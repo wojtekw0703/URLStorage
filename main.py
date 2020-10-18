b3f4700c20f9d15ab7058caaf41529b79e49176d
Wojciech Wydmuch <wojciechwydmuch73@gmail.com>
2020-10-18 22:26:03 +0200

    Add listing feature to the terminal

--- main.py
+++ main.py
@@ -1,35 +1,34 @@
+from database import Database
 from sys import argv
-import sqlite3
 from os import getenv
 from dotenv import load_dotenv
 load_dotenv()
 
 
-class Database:
-    def __init__(self,database_name):
-        self.connection = sqlite3.connect(database_name)
-        self.cursor = self.connection.cursor()
-
-    def __del__(self):
-        self.connection.close()
-
-    def create_table(self, sql: str):
-        self.cursor.execute(sql)
-        self.connection.commit()
-
-    def insert(self, table, *values):
-        self.cursor.execute(f"INSERT INTO {table} VALUES ({','.join(['?' for _ in values])})",values)
-        self.connection.commit()
-
 if len(argv) == 2 and argv[1] == "setup":
+    '''
+    Initialization of the database
+    Usage: python main.py setup
+    '''
     print('Tworze tabele')
     db = Database(getenv("DB_NAME"))
     db.create_table('CREATE TABLE urls (id INTEGER PRIMARY KEY AUTOINCREMENT, category TEXT, url TEXT)')
 
 if len(argv) == 4 and argv[1] == 'add':
+    '''
+    Adding new resource
+    Usage:  pythonmain.py add http://wojciechwydmuch.com
+    '''
     print('Dodaje nowy adres url')
     category = argv[2]
     url = argv[3]
     db = Database(getenv("DB_NAME"))
     db.insert('urls', None, category, url)
 
+if len(argv) == 3 and argv[1] == 'list':
+    print('Lista linkow z kategorii:')
+    category = argv[2]
+    db = Database(getenv("DB_NAME"))
+    links = db.fetch_all('urls',category=category)
+    for link in links:
+        print(link)
